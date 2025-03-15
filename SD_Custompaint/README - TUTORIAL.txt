1f489ccf222b3906d39b788ebf348c947067b99c
SouthDevs <southdevs.release@gmail.com>
2025-03-15 18:29:54 +0100

    Add files via upload

---
+++ SD_Custompaint/README - TUTORIAL.txt
@@ -0,0 +1,12 @@
+Come visit us on our discord, even just to say hello or... follow us!
+https://discord.gg/6Yy9qmrW5Q
+
+
+
+THERE IS A MEGA GIVEAWAY RUNNING HERE (03-13-2025 to 03-31-2025)
+https://discord.gg/yvM9VREmnM
+
+
+
+Documentation-tutorial
+https://docs.google.com/document/d/1MjXOrfjO1ZRaifIwh2GNc2tW6G1Ybs6pwWoY7XvSim4/edit?usp=sharing
---
+++ SD_Custompaint/fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version "adamant"
+game "rdr3"
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+
+author 'SouthDevs'
+description 'SD_Custmpaint'
+version '1.0.0'
+lua54 'yes'
+
+this_is_a_map "yes"
+
+
+files {
+'stream/sd_custompaint_golden_big.ytyp',
+'stream/sd_custompaint_wooden_big.ytyp',
+'stream/sd_custompaint_wooden_medium.ytyp'
+
+
+
+}
+
+
+
+data_file 'DLC_ITYP_REQUEST' 'sd_custompaint_golden_big.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'sd_custompaint_wooden_big.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'sd_custompaint_wooden_medium.ytyp'
+
+
+
